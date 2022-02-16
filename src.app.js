799b501698c1599580ec3a55096af7c376647b62
JasminDior <95191285+JasminDior@users.noreply.github.com>
2022-02-15 19:25:13 -0500

    recreated js file

--- src.app.js
+++ src.app.js
@@ -1,9 +1,9 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(respomse.data.main.temp);
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
 
 let apiKey = "15dc396ba08536ce424f26fbc76e79a9";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Raleigh&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Raleigh&appid=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
