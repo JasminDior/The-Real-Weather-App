f0696491ff8b781773745717b6ef7b22c5cba057
JasminDior <95191285+JasminDior@users.noreply.github.com>
2022-02-15 19:49:25 -0500

    updates to li items

--- src.app.js
+++ src.app.js
@@ -1,6 +1,14 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  let descriptionElement = document.querySelector("#description");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  humidityElement.innerHTML = response.data.main.humidity;
+  description.innerHTML = response.data.weather[0].description;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "15dc396ba08536ce424f26fbc76e79a9";
